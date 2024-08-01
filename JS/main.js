dbaa54845bbe9b1e4684cfc1cd0c79a62987d818
KimDuNa <anoldlee00@naver.com>
2024-08-01 12:25:04 +0900

    3

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,24 @@
-//mainImg 계속 돌리기
+document.addEventListener("DOMContentLoaded", () => {
+  const skills = [
+    "HTML",
+    "CSS",
+    "JAVASCRIPT",
+    "REACT",
+    "PHOTOSOP",
+    "Illustrat",
+    "Git Hub",
+    "Figma",
+  ];
+
+  const skillDiv = document.querySelector("#skill");
+  const marqueeUl = document.createElement("ul");
+
+  skills.forEach((skill) => {
+    const li = document.createElement("li");
+    li.textContent = skill;
+    marqueeUl.appendChild(li);
+  });
+  const firstLi = marqueeUl.firstElementChild.cloneNode(true);
+  marqueeUl.appendChild(firstLi);
+  skillDiv.appendChild(marqueeUl);
+});
