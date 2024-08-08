6e030c12ad469c4995838ceba52dc4db1641aaaa
김두나 <2023108143@hycu.ac.kr>
2024-08-08 15:24:05 +0900

    11

--- JS/main.js
+++ JS/main.js
@@ -1,83 +1,9 @@
-/*
-document.addEventListener("DOMContentLoaded", function () {
-  const marquee = gsap.timeline();
-
-  // .marquee-content 요소를 올바르게 선택하여 애니메이션 적용
-
-  ScrollTrigger.create({
-    animation: marquee,
-    trigger: "#about", // 트리거 요소를 #skill로 설정
-    start: "top 80%",
-    end: "150% 30%",
-    scrub: false,
-    pin: false,
-    markers: false,
-  });
-});
-*/
 const $mousePinter = document.querySelector(".mouseChild");
-document.addEventListener("mousemove", function (e) {
+window.addEventListener("mousemove", function (e) {
   const mouseX = e.clientX;
   const mouseY = e.clientY;
   $mousePinter.style.left = mouseX + "px";
   $mousePinter.style.top = mouseY + "px";
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  // .marquee-content 요소를 선택합니다.
-  const marqueeContent = document.querySelector(".marquee-content");
-
-  const triggerElement = document.querySelector("#about");
-
-  // Intersection Observer 옵션 설정
-  const options = {
-    root: triggerElement, // 뷰포트를 루트로 사용
-    rootMargin: "0px", // 루트의 마진
-    threshold: 0.2, // 80%가 보일 때 콜백 실행
-  };
-
-  // Intersection Observer 콜백 함수
-  const observerCallback = (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        // 애니메이션 로직을 여기에 추가합니다.
-        animateMarquee();
-        observer.unobserve(entry.target); // 애니메이션이 한 번만 실행되도록 설정
-      }
-    });
-  };
-
-  // Intersection Observer 생성
-  const observer = new IntersectionObserver(observerCallback, options);
-
-  // 감시할 요소 설정
-  observer.observe(triggerElement);
-
-  // 애니메이션 함수
-  function animateMarquee() {
-    // 애니메이션 로직
-    let start = null;
-    const duration = 1000; // 애니메이션 지속 시간 (밀리초)
-    const startPos = parseInt(getComputedStyle(marqueeContent).top, 10);
-
-    const endPos = startPos - 1150; // 원하는 위치
-
-    function step(timestamp) {
-      if (!start) start = timestamp;
-      const progress = timestamp - start;
-      const ease = progress / duration; // 단순 선형 이징
-
-      marqueeContent.style.transform = `translateY(${
-        startPos + ease * (endPos - startPos)
-      }px)`;
-
-      if (progress < duration) {
-        window.requestAnimationFrame(step);
-      }
-    }
-
-    window.requestAnimationFrame(step);
-  }
 });
 
 //hambergerMenu
@@ -91,23 +17,82 @@ $hambergetBox.addEventListener("click", function () {
   $hambergerList.classList.toggle("active");
 });
 
-//about
+// myName,skills,nature
+gsap.registerPlugin(ScrollTrigger);
 
-document.addEventListener("DOMContentLoaded", function () {
-  const $myName = document.querySelector(".myName");
-  const $skills = document.querySelector(".skills");
-  const $nature = document.querySelector(".natuer");
-  const $body = document.querySelector("body");
+gsap.to(".myName", {
+  scrollTrigger: {
+    trigger: ".myName",
+    start: "top 80%",
+    end: "top 0%",
+    scrub: true,
+  },
+  x: 500,
+});
 
-  $body.addEventListener("mousemove", function (e) {
-    const x = e.clientY;
-    console.log(x);
+gsap.to(".skills", {
+  scrollTrigger: {
+    trigger: ".skills",
+    start: "top 80%",
+    end: "top 0%",
+    scrub: true,
+  },
+  x: -500,
+});
+
+gsap.to(".nature", {
+  scrollTrigger: {
+    trigger: ".nature",
+    start: "top 80%",
+    end: "top 0%",
+    scrub: true,
+  },
+  x: 500,
+});
+
+// project hide Img
+const projectImgElements = document.querySelectorAll(".projectImg_el");
+
+const projectVisible = (mouseX, mouseY) => {
+  let visibleElement = null;
+
+  projectImgElements.forEach(($projectImgBox) => {
+    const projectImgBoxRect = $projectImgBox.getBoundingClientRect();
+
+    if (
+      mouseX >= projectImgBoxRect.left &&
+      mouseX <= projectImgBoxRect.right &&
+      mouseY >= projectImgBoxRect.top &&
+      mouseY <= projectImgBoxRect.bottom
+    ) {
+      visibleElement = $projectImgBox;
+    } else {
+      // 외부에 있으면 이미지 박스를 숨김
+      gsap.to($projectImgBox, {
+        autoAlpha: 0,
+        duration: 0.2,
+      });
+    }
   });
-  $myName.addEventListener("mousemove", function (e) {
-    const rect = $myName.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const width = rect.width;
-    const perventage = (x / width) * 100;
-    console.log(x);
-  });
+
+  if (visibleElement) {
+    // 내부에 있으면 이미지 박스를 표시하고 위치를 업데이트
+    gsap.to(visibleElement, {
+      x:
+        mouseX -
+        visibleElement.getBoundingClientRect().left -
+        visibleElement.clientWidth / 2,
+      y:
+        mouseY -
+        visibleElement.getBoundingClientRect().top -
+        visibleElement.clientHeight / 2,
+      autoAlpha: 1,
+      duration: 0.2,
+    });
+  }
+};
+
+// 모든 프로젝트 이미지 요소에 대해 이벤트 리스너 추가
+document.addEventListener("mousemove", (e) => {
+  projectVisible(e.clientX, e.clientY);
 });
